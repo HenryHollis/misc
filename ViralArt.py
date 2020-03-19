606b18085e76bb199cefd5dd8455bc174c8df3cb
Henry Hollis <41795183+HenryHollis@users.noreply.github.com>
2020-03-19 09:59:59 -0400

    Add files via upload

---
+++ ViralArt.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+from Bio import SeqIO
+import numpy as np
+from PIL import Image
+import math
+records = []
+colors = {'A' : [41, 41, 106],
+          'G' : [171, 85, 161],
+          'T' : [94, 202, 233],
+          'C' : [111, 87, 164],
+          'U' : [94, 202, 233]
+    
+}   # a nice blue color scheme
+
+
+for record in SeqIO.parse("sequence.fasta", "fasta"):
+    records.append(record) # get records from fasta file
+if records:
+    sequence = records[0].seq
+    length = len(sequence)
+    dim = math.ceil(math.sqrt(length))  # dimensions of our image is the sqrt of seq length
+
+    data = np.zeros((dim, dim, 3), dtype=np.uint8 ) # initialize array of  zeroes
+    for i, base in enumerate(sequence):
+         data[int(i/dim)][i%dim] = colors[base]
+    image = Image.fromarray(data)   #create the image
+    image.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
